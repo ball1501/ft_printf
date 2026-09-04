1b45e36ba0a09d92ff3718c536d7fb0d4aa39dcd
ball1501 <ball_zider@hotmail.com>
2026-09-04 10:49:32 +0700

    add libft

---
+++ ft_printf_char.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_char.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wngamkri <wngamkri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/04 09:23:52 by wngamkri          #+#    #+#             */
+/*   Updated: 2026/09/04 10:41:29 by wngamkri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar_rtn(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int	ft_putstr_rtn(char *str)
+{
+	int	len;
+
+	if (str == NULL)
+	{
+		return (write(1, "(null)", 6));
+	}
+	len = 0;
+	while (str[len])
+		len++;
+	return (write(1, str, len));
+}
