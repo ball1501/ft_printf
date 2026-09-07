43396fd328923635bdfb82c0951c8106b303af7e
ball1501 <ball_zider@hotmail.com>
2026-09-07 14:12:51 +0700

    change int to long

--- ft_printf_nbr.c
+++ ft_printf_nbr.c
@@ -6,27 +6,25 @@
 /*   By: wngamkri <wngamkri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 12:59:02 by wngamkri          #+#    #+#             */
-/*   Updated: 2026/09/07 11:11:32 by wngamkri         ###   ########.fr       */
+/*   Updated: 2026/09/07 13:02:26 by wngamkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr_rtn(int n)
+int	ft_putnbr_rtn(long n)
 {
-	int		len;
-	long	nb;
+	int	len;
 
 	len = 0;
-	nb = n;
-	if (nb < 0)
+	if (n < 0)
 	{
 		len += ft_putchar_rtn('-');
-		nb = -nb;
+		n = -n;
 	}
-	if (nb >= 10)
-		len += ft_putnbr_rtn(nb / 10);
-	len += ft_putchar_rtn(nb % 10 + '0');
+	if (n >= 10)
+		len += ft_putnbr_rtn(n / 10);
+	len += ft_putchar_rtn(n % 10 + '0');
 	return (len);
 }
 
